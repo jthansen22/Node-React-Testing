09e9ee8b6cbfeabc976220773fc8d84a9ea3461c
jthansen22 <jthansen22@gmail.com>
2017-09-10 11:14:02 -0700

    googleOAuth Setup

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// Passport Middleware
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, callback) => {
+      console.log('accessToken');
+      console.log('profile');
+    }
+  )
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // Initiate Server 
 app.listen(PORT);
-
